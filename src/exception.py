6a536f87467b8d6fd3e807a613a517d428dbfbe1
vishwa9868 <vishwanathpatil9868@gmail.com>
2024-03-07 14:42:58 +0530

    data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,31 +1,26 @@
 import logging
 import sys
-import traceback
-
+from typing import Optional, Type
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    error_message = "Error occurred in Python script: {0}, line number: {1}, error message: {2}\n{3}".format(
-        file_name, line_number, str(error), traceback.format_exc()
-    )
+    error_message = "Error occurred in Python script: {0}, line number: {1}, error message: {2}".format(
+        file_name, exc_tb.tb_lineno, str(error))
     return error_message
 
-
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message, error_detail: Optional[Type[BaseException]]):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
 
-
 if __name__ == "__main__":
     try:
         a = 1 / 0
     except Exception as e:
-        raise CustomException("Division by zero error", error_detail=e)
+        raise CustomException("Division by zero error", error_detail=e) from e
 
     logging.info("Logging has started")
